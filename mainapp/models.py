40ea47502c82da51b64e7198a7935cdf07d0b0ac
Rhobar9 <olo.k97@wp.pl>
2022-09-11 15:36:34 +0200

    Module2_length_of_post_fixed

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 
 class BlogPost(models.Model):
-    title = models.CharField(max_length=2000, unique=True)
+    title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE)
     body = models.TextField()
     postdate = models.DateTimeField(auto_now_add=True, blank=True)
