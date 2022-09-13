b7f21c6169d490b01553eaa70719e73e300b5345
Rhobar9 <olo.k97@wp.pl>
2022-09-13 15:19:59 +0200

    Module_3

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('post/<int:id>', views.post, name='post'),
 #    path('user/<int:id>', views.user_posts, name='user_posts'),
-#    path('tag/<str:name>', views.tag_posts, name='tag_posts'),
+    path('tag/<str:name>', views.tag_posts, name='tag_posts'),
     path('', views.index, name='index'),
-] 
+
+]
+
+
